b4c2835137728abd1b798249ad4164862ee69f27
Vlad Dinu <dinuvld@hotmail.com>
2016-11-26 22:09:20 +0200

    - Added the remove_char function to the String class - Aded len function suport to String

--- string.py
+++ string.py
@@ -1,7 +1,21 @@
+import search
+
 class String:
 
     def __init__(self, string):
         self.string = string
 
-    def remove_char(self, start, end):
-        for index in range(0, string.len)
+    def __len__(self):
+        return len(self.string)
+
+    def remove_char(self, position):
+        """Takes the position of a character in a string (starting from 0) and  deletes at that position"""
+        newstr = list()
+        for index in range(0, position):
+            newstr.append(self.string[index])
+        for index in range(position + 1, len(self.string)):
+            newstr.append(self.string[index])
+        self.string = "".join(newstr)
+
+
+
