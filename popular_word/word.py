c4b52e4db6421e7964e916143a4ecf201dbacae9
Vlad Dinu <dinuvld@hotmail.com>
2016-11-29 22:36:14 +0200

    Introduced a bug in Word.evaluate_string(), getting an out of index error only for some strings for no apparent reason, will look more into it

--- popular_word/word.py
+++ popular_word/word.py
@@ -8,7 +8,22 @@ class Word(String):
     def __init__(self, text):
         self.text = text
 
-    def evaluate_word(self, word):
+    def evaluate_word(self):
+        """Check in order to see if a word contains any invalid characters"""
+
         newstr = list()
-        for index in range(0, word):
-            
+        for index in range(0, len(self.text) - 1):
+           # print len(self.text)
+            #print self.text
+            if (self.text[index] in self.bad_chars):
+                self.delete_char(index)
+                print index
+
+
+
+str = raw_input()
+str = Word(str)
+str.evaluate_word()
+print str.text
+
+
